6a8ce91d0866931181d3a2947ec9c4a10ef606fa
justinchen <justinchen>
2006-06-01 22:57:24 +0000

    Relate to fixes for bug 144954, improve efficiency when constructing jar path collection.

--- jsf/plugins/org.eclipse.jst.jsf.core/src/org/eclipse/jst/jsf/core/internal/project/facet/JSFUtils.java
+++ jsf/plugins/org.eclipse.jst.jsf.core/src/org/eclipse/jst/jsf/core/internal/project/facet/JSFUtils.java
@@ -330,11 +330,10 @@ public class JSFUtils {
 		EList archiveFiles = jsfLib.getArchiveFiles();
 		int numJars = archiveFiles.size();
 		String name = null;		
-		IPath[] elements = new IPath[numJars];				
+		IPath[] elements = new IPath[numJars];
 		for (int i= 0; i < numJars; i++) {
-			name = ((ArchiveFile)archiveFiles.get(i)).getName();
-			elements[i] = new Path(((ArchiveFile)archiveFiles.get(i)).getPath() + IPath.SEPARATOR + name).makeAbsolute();	
-		}			
+			elements[i] = new Path(((ArchiveFile)archiveFiles.get(i)).getResolvedSourceLocation()).makeAbsolute();	
+		}
 		return elements;
 	}		
 	
