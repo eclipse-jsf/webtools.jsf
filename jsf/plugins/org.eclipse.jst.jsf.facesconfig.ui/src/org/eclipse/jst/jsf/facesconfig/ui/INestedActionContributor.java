4fcad312612c63eb7ad362f067fb432aafc35c7d
rsrinivasan <rsrinivasan>
2006-06-19 22:39:06 +0000

    This commit was manufactured by cvs2svn to create tag 'v20060620'.

--- jsf/plugins/org.eclipse.jst.jsf.facesconfig.ui/src/org/eclipse/jst/jsf/facesconfig/ui/INestedActionContributor.java
+++
@@ -1,9 +0,0 @@
-package org.eclipse.jst.jsf.facesconfig.ui;
-
-/**
- * The nested action contributor should be able to update its actions as
- * required.
- */
-public interface INestedActionContributor {
-	void update();
-}
