1d2f1ff407a346d057b7ff6490f14662b335a97f
cbateman <cbateman>
2007-01-24 01:12:44 +0000

    Removed dead code.

--- jsf/plugins/org.eclipse.jst.pagedesigner/src/org/eclipse/jst/pagedesigner/requests/LocationModifierRequest.java
+++ jsf/plugins/org.eclipse.jst.pagedesigner/src/org/eclipse/jst/pagedesigner/requests/LocationModifierRequest.java
@@ -21,8 +21,6 @@ import org.eclipse.gef.requests.LocationRequest;
  */
 public class LocationModifierRequest extends LocationRequest {
 
-	// TODO: dead? private int _statemask;
-
 	private boolean _controlKeyDown;
 
 	/**
@@ -30,7 +28,6 @@ public class LocationModifierRequest extends LocationRequest {
 	 */
 	public LocationModifierRequest() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 
 	/**
@@ -47,60 +44,4 @@ public class LocationModifierRequest extends LocationRequest {
 	public void setControlKeyPressed(boolean b) {
 		this._controlKeyDown = b;
 	}
-	// /**
-	// * Returns <code>true</code> if the ALT key is currently pressed.
-	// * @return whether the ALT key is pressed
-	// */
-	// public boolean isAltKeyPressed() {
-	// return ((_statemask & MouseEvent.ALT) != 0);
-	// }
-	//
-	// /**
-	// * Returns <code>true</code> if any mouse button is currently pressed.
-	// * @return whether any mouse button is pressed
-	// */
-	// public boolean isAnyMouseButtonPressed() {
-	// return ((_statemask & MouseEvent.ANY_BUTTON) != 0);
-	// }
-	//
-	// /**
-	// * Returns <code>true</code> if the CTRL key is currently pressed.
-	// * @return whether the CTRL key is pressed
-	// */
-	// public boolean isControlKeyPressed() {
-	// return ((_statemask & MouseEvent.CONTROL) != 0);
-	// }
-	//
-	// /**
-	// * Returns <code>true</code> if the left mouse button is pressed.
-	// * @return whether the left mouse button is pressed
-	// */
-	// public boolean isLeftMouseButtonPressed() {
-	// return ((_statemask & MouseEvent.BUTTON1) != 0);
-	// }
-	//
-	// /**
-	// * Returns <code>true</code> if the right mouse button is pressed.
-	// * @return whether the right mouse button is pressed
-	// */
-	// public boolean isRightMouseButtonPressed() {
-	// return ((_statemask & MouseEvent.BUTTON3) != 0);
-	// }
-	//
-	// /**
-	// * Returns <code>true</code> if the SHIFT key is currently pressed.
-	// * @return whether the SHIFT key is pressed
-	// */
-	// public boolean isShiftKeyPressed() {
-	// return ((_statemask & MouseEvent.SHIFT) != 0);
-	// }
-	//
-	// /**
-	// * Sets the statemask for this request.
-	// * @param mask the statemask
-	// */
-	// public void setModifiers(int mask) {
-	// this._statemask = mask;
-	// }
-
 }
