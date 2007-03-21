659e35153b08621873d1cea1b0d9e0ed95df8d12
rsrinivasan <rsrinivasan>
2007-03-21 17:44:52 +0000

    This commit was manufactured by cvs2svn to create tag 'v20070321'.

--- jsf/plugins/org.eclipse.jst.jsf.common/src/org/eclipse/jst/jsf/common/metadata/internal/IClassLoaderProvider.java
+++
@@ -1,9 +0,0 @@
-package org.eclipse.jst.jsf.common.metadata.internal;
-
-public interface IClassLoaderProvider {
-	/**
-	 * @param className
-	 * @return Class - implementers should eat exceptions and return null whenever class cannot be returned
-	 */
-	public Class loadClass(String className);
-}
--- jsf/plugins/org.eclipse.jst.jsf.core/src/org/eclipse/jst/jsf/taglibprocessing/internal/provisional/attributevalues/FacesConfigConverterIDType.java
+++
@@ -1,7 +0,0 @@
-package org.eclipse.jst.jsf.taglibprocessing.internal.provisional.attributevalues;
-
-/**
- * Type representing a FacesConfig Converter attribute
- */
-public class FacesConfigConverterIDType extends FacesConfigIdentifierType {
-}
