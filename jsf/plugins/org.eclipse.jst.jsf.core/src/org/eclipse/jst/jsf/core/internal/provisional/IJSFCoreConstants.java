2a57e4f0f29efe8c2c433c526994c31fe9942259
cbateman <cbateman>
2007-04-04 18:58:42 +0000

    Adding.

---
+++ jsf/plugins/org.eclipse.jst.jsf.core/src/org/eclipse/jst/jsf/core/internal/provisional/IJSFCoreConstants.java
@@ -0,0 +1,48 @@
+package org.eclipse.jst.jsf.core.internal.provisional;
+
+/**
+ * JSF Core framework constants
+ * 
+ * @author cbateman
+ *
+ */
+public final class IJSFCoreConstants 
+{
+    /**
+     * The global id for the JSF facet
+     * TODO: align with extensioin point through plugin.properties
+     */
+    public static final String JSF_CORE_FACET_ID = "jst.jsf"; //$NON-NLS-1$
+    /**
+     * The facet version for a JSF 1.0 project
+     * TODO: align with extensioin point through plugin.properties
+     */
+    public final static String                  FACET_VERSION_1_0 = "1.0"; //$NON-NLS-1$
+    /**
+     * The constant id for a JSF 1.0 project
+     */
+    public final static String                  JSF_VERSION_1_0 = FACET_VERSION_1_0;
+    /**
+     * The facet version for a JSF 1.1 project
+     * TODO: align with extensioin point through plugin.properties
+     */
+    public final static String                  FACET_VERSION_1_1 = "1.1"; //$NON-NLS-1$
+    /**
+     * The constant id for a JSF 1.1 project
+     */
+    public final static String                  JSF_VERSION_1_1 = FACET_VERSION_1_1;
+    /**
+     * The facet version for a JSF 1.2 project
+     * TODO: align with extensioin point through plugin.properties
+     */
+    public final static String                  FACET_VERSION_1_2 = "1.2";//$NON-NLS-1$
+    /**
+     * The constant id for a JSF 1.2 project
+     */
+    public final static String                  JSF_VERSION_1_2 = FACET_VERSION_1_2;
+    
+    private IJSFCoreConstants()
+    {
+        // no instantiation
+    }
+}
