c4c9e18bf1b860238fb13e8b0d91ce806432032d
cbateman <cbateman>
2007-04-16 17:50:49 +0000

    Added comments to public constants to resolve Javadoc warnings.

--- jsf/plugins/org.eclipse.jst.jsf.core/src/org/eclipse/jst/jsf/taglibprocessing/internal/provisional/attributevalues/Messages.java
+++ jsf/plugins/org.eclipse.jst.jsf.core/src/org/eclipse/jst/jsf/taglibprocessing/internal/provisional/attributevalues/Messages.java
@@ -21,29 +21,100 @@ public class Messages extends NLS {
 	static {
 		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
 	}
+	/**
+	 * see messages.properties
+	 */
 	public static String ActionType_navcase_display;
+    /**
+     * see messages.properties
+     */
 	public static String ActionType_invalid_value;
+    /**
+     * see messages.properties
+     */
 	public static String BooleanType_invalid_values;
+    /**
+     * see messages.properties
+     */
 	public static String ComponentBindingType_invalid_value;
+    /**
+     * see messages.properties
+     */
 	public static String ComponentIDType_invalid_as_el;
+    /**
+     * see messages.properties
+     */
 	public static String ComponentIDType_invalid_value;
+    /**
+     * see messages.properties
+     */
 	public static String DoubleType_invalid_double;
+    /**
+     * see messages.properties
+     */
 	public static String DoubleType_invalid_member;
+    /**
+     * see messages.properties
+     */
 	public static String FacesConfigConverterIDFeatures_converterid_empty;
+    /**
+     * see messages.properties
+     */
 	public static String FacesConfigIdentifierType_invalid_converter_id;
+    /**
+     * see messages.properties
+     */
 	public static String FacesConfigIdentifierType_invalid_validator_id;
+    /**
+     * see messages.properties
+     */
 	public static String FacesConfigValidatorIDFeatures_validatorid_empty;
+    /**
+     * see messages.properties
+     */
 	public static String IntegerType_invalid_integer;
+    /**
+     * see messages.properties
+     */
 	public static String IntegerType_invalid_member;
+    /**
+     * see messages.properties
+     */
 	public static String JavaClassType_invalid_type;
+    /**
+     * see messages.properties
+     */
 	public static String JavaClassType_not_found;
+    /**
+     * see messages.properties
+     */
 	public static String LongType_invalid_long;
+    /**
+     * see messages.properties
+     */
 	public static String LongType_invalid_member;
+    /**
+     * see messages.properties
+     */
 	public static String MethodBindingType_invalid_value;
+    /**
+     * see messages.properties
+     */
 	public static String NumberType_max_val;
+    /**
+     * see messages.properties
+     */
 	public static String NumberType_min_val;
+    /**
+     * see messages.properties
+     */
 	public static String StringType_invalid_value;
+    /**
+     * see messages.properties
+     */
 	public static String ValueType_invalid_value;
+    /**
+     * see messages.properties
+     */
 	public static String ValueType_invalid_value_without_setter;
-	
 }
