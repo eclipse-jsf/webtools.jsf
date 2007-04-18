a66173a0820eb02317947f35587df3bd4196ac8b
gkessler <gkessler>
2007-04-18 15:18:03 +0000

    Fix JUnit failures after package refactoring (2)

--- jsf/plugins/org.eclipse.jst.jsf.core/src/org/eclipse/jst/jsf/metadataprocessors/internal/AttributeValueRuntimeTypeRegistry.java
+++ jsf/plugins/org.eclipse.jst.jsf.core/src/org/eclipse/jst/jsf/metadataprocessors/internal/AttributeValueRuntimeTypeRegistry.java
@@ -22,7 +22,7 @@ package org.eclipse.jst.jsf.metadataprocessors.internal;
 public class AttributeValueRuntimeTypeRegistry extends AbstractMetaDataEnabledTypeRegistry {
 
 	private static final String EXTPTID = "AttributeValueRuntimeTypes";
-	private static final String DEFAULT_CLASS = "org.eclipse.jst.jsf.metadataprocessors.internal.provisional.DefaultTypeDescriptor";
+	private static final String DEFAULT_CLASS = "org.eclipse.jst.jsf.metadataprocessors.DefaultTypeDescriptor";
 	private static AttributeValueRuntimeTypeRegistry INSTANCE;
 	
 	/**
