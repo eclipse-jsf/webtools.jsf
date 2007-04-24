357adf84615c1997fa58b2afc9da49974b91fc97
rsrinivasan <rsrinivasan>
2007-04-24 19:09:57 +0000

    This commit was manufactured by cvs2svn to create tag 'v20070424'.

--- jsf/plugins/org.eclipse.jst.jsf.core/src/org/eclipse/jst/jsf/taglibprocessing/attributevalues/FacesConfigConverterIDType.java
+++
@@ -1,8 +0,0 @@
-package org.eclipse.jst.jsf.taglibprocessing.attributevalues;
-
-/**
- * Type representing a FacesConfig Converter attribute
- */
-public class FacesConfigConverterIDType extends FacesConfigIdentifierType {
-    // FIXME: why do we have an empty class?
-}
--- jsf/plugins/org.eclipse.jst.jsf.core/src/org/eclipse/jst/jsf/taglibprocessing/attributevalues/FacesConfigValidatorIDType.java
+++
@@ -1,8 +0,0 @@
-package org.eclipse.jst.jsf.taglibprocessing.attributevalues;
-
-/**
- * Type representing a FacesConfig Validator attribute
- */
-public class FacesConfigValidatorIDType extends FacesConfigIdentifierType {
-    // FIXME: why do we have an empty class?
-}
