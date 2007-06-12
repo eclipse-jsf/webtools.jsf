1664c91f02c0aca280e8dbd5bd67c55fe4ef69f9
gkessler <gkessler>
2007-06-12 21:41:56 +0000

    [Bug 191564] Design flaw in metadata 'include-entity-group' functionality

--- jsf/tests/org.eclipse.jst.jsf.metadata.tests/src/org/eclipse/jst/jsf/common/metadata/tests/AbstractBaseMetaDataTestCase.java
+++ jsf/tests/org.eclipse.jst.jsf.metadata.tests/src/org/eclipse/jst/jsf/common/metadata/tests/AbstractBaseMetaDataTestCase.java
@@ -10,11 +10,18 @@
  *******************************************************************************/
 package org.eclipse.jst.jsf.common.metadata.tests;
 
+import java.util.Iterator;
+import java.util.List;
+
 import junit.framework.TestCase;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.Path;
+import org.eclipse.jst.jsf.common.metadata.Entity;
+import org.eclipse.jst.jsf.common.metadata.Trait;
+import org.eclipse.jst.jsf.common.metadata.internal.TraitValueHelper;
+import org.eclipse.jst.jsf.common.metadata.traittypes.traittypes.ListOfValues;
 import org.eclipse.jst.jsf.context.structureddocument.IStructuredDocumentContext;
 import org.eclipse.jst.jsf.context.structureddocument.IStructuredDocumentContextFactory;
 import org.eclipse.jst.jsf.metadata.tests.MetadataTestsPlugin;
@@ -26,7 +33,6 @@ import org.eclipse.wst.sse.core.internal.provisional.IModelManager;
 import org.eclipse.wst.sse.core.internal.provisional.IStructuredModel;
 
 public abstract class AbstractBaseMetaDataTestCase extends TestCase {
-
 	protected static final String projName = "TestCommonMetadataProject";
 	protected static final String domain = "TagLibraryDomain";
 	protected static final String badDomain = "TagLibDomain";
@@ -39,11 +45,19 @@ public abstract class AbstractBaseMetaDataTestCase extends TestCase {
 	protected IProject project;
 	protected WebProjectTestEnvironment projectTestEnvironment;
 	protected IStructuredDocumentContext docContext;
+
+	private boolean debug_info = false;
+	private long startTime;
+	private String debugTitle;
 	
 	public void setUp() throws Exception {
 	    super.setUp();
 	    
+	    debug_info = false;
+	    
 	    JSFTestUtil.setInternetProxyPreferences(true, "www-proxy.us.oracle.com", "80");
+	    
+	    JSFTestUtil.setValidationEnabled(false);
 	    
 	    projectTestEnvironment = 
 	        new WebProjectTestEnvironment(projName/*+"_"+getClass().getName()+"_"+getName()*/);
@@ -86,5 +100,61 @@ public abstract class AbstractBaseMetaDataTestCase extends TestCase {
 				model.getStructuredDocument(), offset);
 		
 	}
+    
+	protected void showDebugInfo(boolean show){
+		debug_info = show;
+	}
+	
+	protected void dumpMDTree(Entity entity, int indent) {
+		if (debug_info){
+			printLine("Entity: "+entity.getId(),indent);
+			indent++;
+			for(Iterator it=entity.getTraits().iterator();it.hasNext();){
+				Trait t = (Trait)it.next();
+				printLine("Trait: "+t.getId()+"["+ getValue(t)+ "]", indent);
+			}
+			for (Iterator it=entity.getChildEntities().iterator();it.hasNext();){
+				dumpMDTree((Entity)it.next(), indent);
+			}
+		}
+	}
+	
+	private String getValue(Trait trait) {
+		if (trait.getValue() instanceof ListOfValues){
+			List l = TraitValueHelper.getValueAsListOfStrings(trait);
+			StringBuffer buf = new StringBuffer();
+			for (Iterator it=l.iterator();it.hasNext();){				
+				buf.append((String)it.next());
+				buf.append(", ");
+			}
+			if (buf.toString().length() > 0)
+				return buf.toString().substring(0, buf.toString().length() - 2);
+			return null;
+		}
+		return 	TraitValueHelper.getValueAsString(trait);
+	}
 
+	private void printLine(String line, int indent) {
+		StringBuffer buf = new StringBuffer();
+		for (int i=0;i<indent;i++){
+			buf.append("    ");
+		}
+		buf.append(line);
+		System.out.println(buf.toString());
+	}
+
+	protected void endTime() {
+		if (debug_info){
+			long delta = System.nanoTime() - startTime;
+			printLine("Time for "+debugTitle+"(ms)= "+String.valueOf(delta/1000000), 0);
+		}	
+	}
+
+	protected void startTime(String debugTitle) {
+		if (debug_info){
+			this.debugTitle = debugTitle;
+			startTime = System.nanoTime();
+			printLine("",0);
+		}		
+	}
 }
--- jsf/tests/org.eclipse.jst.jsf.metadata.tests/src/org/eclipse/jst/jsf/common/metadata/tests/MergeTests.java
+++ jsf/tests/org.eclipse.jst.jsf.metadata.tests/src/org/eclipse/jst/jsf/common/metadata/tests/MergeTests.java
@@ -13,21 +13,67 @@ package org.eclipse.jst.jsf.common.metadata.tests;
 import org.eclipse.jst.jsf.common.metadata.Entity;
 import org.eclipse.jst.jsf.common.metadata.Model;
 import org.eclipse.jst.jsf.common.metadata.Trait;
-import org.eclipse.jst.jsf.common.metadata.internal.TaglibDomainMetaDataModelContextImpl;
 import org.eclipse.jst.jsf.common.metadata.internal.TraitValueHelper;
 import org.eclipse.jst.jsf.common.metadata.query.ITaglibDomainMetaDataModelContext;
 import org.eclipse.jst.jsf.common.metadata.query.TaglibDomainMetaDataQueryHelper;
 
 public class MergeTests extends AbstractBaseMetaDataTestCase {
+	private boolean _debugInfo = false;
 	
-	public void testMergeOfFileAandFileB(){
-		String uri =  "http://org.eclipse.jsf/mergetest1";
+	public void testIncludeMerge(){
+		showDebugInfo(_debugInfo);
+		String uri =  "TinyIncludeTest";
 		ITaglibDomainMetaDataModelContext modelContext 
 			= TaglibDomainMetaDataQueryHelper.createMetaDataModelContext(project, uri);
+		startTime(uri);
 		Model  model = TaglibDomainMetaDataQueryHelper.getModel(modelContext);
+		endTime();
 		assertNotNull(model);
 		
-		//check model traits
+		dumpMDTree(model, 0);
+				
+		//expect 0 model traits and 1 entity
+		assertEquals(0, model.getTraits().size());
+		assertEquals(1, model.getChildEntities().size());	
+		
+		
+		//expect 2 traits on A
+		Entity A_Entity = TaglibDomainMetaDataQueryHelper.getEntity(model, "A");
+		assertEquals(2, A_Entity.getTraits().size());
+		Trait t = TaglibDomainMetaDataQueryHelper.getTrait(A_Entity, "T1");
+		assertNotNull(t);
+		assertEquals("A1", TraitValueHelper.getValueAsString(t));
+		t = TaglibDomainMetaDataQueryHelper.getTrait(A_Entity, "T2");
+		assertNotNull(t);
+		assertEquals("T2FromEG", TraitValueHelper.getValueAsString(t));
+		
+		//expect 2 child entities for A
+		assertEquals(2, A_Entity.getChildEntities().size());		
+		Entity a_Entity = TaglibDomainMetaDataQueryHelper.getEntity(A_Entity, "a");
+		assertNotNull(a_Entity);
+		assertEquals(1, a_Entity.getTraits().size());
+		assertEquals(2, a_Entity.getChildEntities().size());	
+		Entity subA_Entity = TaglibDomainMetaDataQueryHelper.getEntity(A_Entity, "sub-A");
+		assertNotNull(subA_Entity);
+		Entity subSubA_Entity = TaglibDomainMetaDataQueryHelper.getEntity(subA_Entity, "sub-sub-A");
+		assertNotNull(subSubA_Entity);
+		
+	}
+	
+
+
+	public void testMergeOfFileAandFileB(){
+		showDebugInfo(_debugInfo);
+		String uri =  "http://org.eclipse.jsf/mergetest1";
+		ITaglibDomainMetaDataModelContext modelContext = TaglibDomainMetaDataQueryHelper.createMetaDataModelContext(project, uri);
+		startTime(uri);
+		Model  model = TaglibDomainMetaDataQueryHelper.getModel(modelContext);
+		assertNotNull(model);
+		endTime();
+		
+		dumpMDTree(model, 0);
+		
+		//check model traits - 
 		assertEquals(3, model.getTraits().size());
 		Trait trait = TaglibDomainMetaDataQueryHelper.getTrait(model, "Dupe");
 		assertNotNull(trait);
@@ -50,12 +96,12 @@ public class MergeTests extends AbstractBaseMetaDataTestCase {
 		trait = TaglibDomainMetaDataQueryHelper.getTrait(entity, "B1");
 		assertNotNull(trait);
 		
-		//check merge of entites within entities
+		//check merge of entities within entities
 		assertEquals(2, entity.getChildEntities().size());
 		Entity secondEntity  = TaglibDomainMetaDataQueryHelper.getEntity(entity, "a");
 		assertNotNull(secondEntity);
 		assertEquals(2, secondEntity.getTraits().size());
-		trait = trait = TaglibDomainMetaDataQueryHelper.getTrait(secondEntity, "A1");
+		trait = TaglibDomainMetaDataQueryHelper.getTrait(secondEntity, "A1");
 		assertNotNull(trait);
 		assertEquals("A1FromA", TraitValueHelper.getValueAsString(trait));
 		
@@ -74,10 +120,13 @@ public class MergeTests extends AbstractBaseMetaDataTestCase {
 		assertEquals(2, entity.getChildEntities().size());
 		secondEntity  = TaglibDomainMetaDataQueryHelper.getEntity(entity, "a");
 		assertNotNull(secondEntity);
-		assertEquals(1, secondEntity.getTraits().size());//do not expect 2 as extra trait from include group is not inlcuded
+		//assertEquals(1, secondEntity.getTraits().size());//do not expect 2 as extra trait from include group is not included
+		assertEquals(2, secondEntity.getTraits().size());//do NOW expect 2 as extra trait from include group IS included (https://bugs.eclipse.org/bugs/show_bug.cgi?id=191564)
 		trait = TaglibDomainMetaDataQueryHelper.getTrait(entity, "A1");
 		assertNotNull(trait);
 		assertEquals("A1", TraitValueHelper.getValueAsString(trait));
+		trait = TaglibDomainMetaDataQueryHelper.getTrait(entity, "A1");
+		assertNotNull(trait);
 		
 		//check merge of entities with includes where all come from include
 		entity = TaglibDomainMetaDataQueryHelper.getEntity(model, "DupeWithInclude2");
@@ -87,7 +136,7 @@ public class MergeTests extends AbstractBaseMetaDataTestCase {
 		assertNotNull(trait);
 		assertEquals("A1FromEG1", TraitValueHelper.getValueAsString(trait));
 		
-		assertEquals(1, entity.getChildEntities().size());
+		assertEquals(1, entity.getChildEntities().size()); 
 		secondEntity  = TaglibDomainMetaDataQueryHelper.getEntity(entity, "a");
 		assertNotNull(secondEntity);
 		assertEquals(2, secondEntity.getTraits().size());
@@ -96,16 +145,20 @@ public class MergeTests extends AbstractBaseMetaDataTestCase {
 		assertEquals("A1FromEG1", TraitValueHelper.getValueAsString(trait));
 
 	}
-	
+
 	/**
-	 * All entites and traits come from external model in this test
+	 * All entities and traits come from external model in this test
 	 */
 	public void testIncludeExternalModel(){
+		showDebugInfo(_debugInfo);
 		String uri =  "http://org.eclipse.jsf/mergetest2";
-		ITaglibDomainMetaDataModelContext modelContext 
-			= TaglibDomainMetaDataQueryHelper.createMetaDataModelContext(project, uri);
+		ITaglibDomainMetaDataModelContext modelContext = TaglibDomainMetaDataQueryHelper.createMetaDataModelContext(project, uri);
+		startTime(uri);
 		Model  model = TaglibDomainMetaDataQueryHelper.getModel(modelContext);
+		endTime();
 		assertNotNull(model);
+		
+		dumpMDTree(model, 0);
 		
 		//check model traits
 		assertEquals(1, model.getTraits().size());
@@ -119,5 +172,31 @@ public class MergeTests extends AbstractBaseMetaDataTestCase {
 		assertNotNull(entity);
 		assertEquals(0, entity.getChildEntities().size());
 		assertEquals(2, entity.getTraits().size());
+	}
+
+	public void testLoadJSFCore(){
+		showDebugInfo(_debugInfo);
+		String uri =  "http://java.sun.com/jsf/core";
+		ITaglibDomainMetaDataModelContext modelContext = 
+			TaglibDomainMetaDataQueryHelper.createMetaDataModelContext(project, uri);
+		startTime(uri);
+		Model  model = TaglibDomainMetaDataQueryHelper.getModel(modelContext);
+		endTime();
+		assertNotNull(model);
+		
+//		dumpMDTree(model, 0);
+	}
+	
+	public void testLoadJSFHTML(){
+		showDebugInfo(_debugInfo);
+		String uri =  "http://java.sun.com/jsf/html";
+		ITaglibDomainMetaDataModelContext modelContext = 
+			TaglibDomainMetaDataQueryHelper.createMetaDataModelContext(project, uri);
+		startTime(uri);
+		Model  model = TaglibDomainMetaDataQueryHelper.getModel(modelContext);
+		endTime();
+		assertNotNull(model);
+		
+//		dumpMDTree(model, 0);
 	}
 }
