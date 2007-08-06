cd9e478bf6718e0e6853f03acdfb1a9df421bb57
cbateman <cbateman>
2007-08-06 23:54:50 +0000

    Added cleanup code for when a bundle source is deleted and added regression test.

--- jsf/tests/org.eclipse.jst.jsf.designtime.tests/src/org/eclipse/jst/jsf/designtime/tests/TestResourceBundleMapSource.java
+++ jsf/tests/org.eclipse.jst.jsf.designtime.tests/src/org/eclipse/jst/jsf/designtime/tests/TestResourceBundleMapSource.java
@@ -12,11 +12,19 @@
 package org.eclipse.jst.jsf.designtime.tests;
 
 import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import junit.framework.TestCase;
 
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jst.jsf.designtime.internal.symbols.ResourceBundleMapSourceFactory;
 import org.eclipse.jst.jsf.test.util.JDTTestEnvironment;
 import org.eclipse.jst.jsf.test.util.JSFTestUtil;
@@ -40,6 +48,9 @@ public class TestResourceBundleMapSource extends TestCase
 
     private IProject   _project1;
     private IProject   _project2;
+    private IFile      _bundle1;
+    private IFile      _bundle2;
+    
     
     protected void setUp() throws Exception 
     {
@@ -47,36 +58,45 @@ public class TestResourceBundleMapSource extends TestCase
         
         JSFTestUtil.setValidationEnabled(false);
         
-        _project1 = initProject("BundleResourceTestProject1", 
+        List<IResource> resources = initProject("BundleResourceTestProject1", 
                                 "/testdata/bundle1.resources.data",
                                 BUNDLE1_PATH, BUNDLE1_NAME);
-        _project2 = initProject("BundleResourceTestProject2", 
+        _bundle1 = (IFile) resources.get(0);
+        _project1 = (IProject) resources.get(1);
+        
+        resources = initProject("BundleResourceTestProject2", 
                                 "/testdata/bundle2.resources.data",
                                 BUNDLE1_PATH, BUNDLE1_NAME);
+        _bundle2 = (IFile) resources.get(0);
+        _project2 = (IProject) resources.get(1);
     }
 
-    private IProject initProject(final String projectName, final String dataFilePath,
+    private List<IResource> initProject(final String projectName, final String dataFilePath,
                                  final String bundlePath, final String bundleName) 
                                     throws Exception
     {
+        final List<IResource>   resources = new ArrayList<IResource>();
         final ProjectTestEnvironment  testEnv = new ProjectTestEnvironment(projectName);
         testEnv.createProject(false);
         final JDTTestEnvironment jdtTestEnv = new JDTTestEnvironment(testEnv);
         final TestFileResource input = new TestFileResource();
         input.load(DesignTimeTestsPlugin.getDefault().getBundle(), 
                    dataFilePath);
-        jdtTestEnv.addResourceFile("src", new ByteArrayInputStream(input.toBytes()), 
-                                    bundlePath, bundleName);
+        resources.add(jdtTestEnv.addResourceFile("src", new ByteArrayInputStream(input.toBytes()), 
+                                    bundlePath, bundleName));
+        
         IProject project = testEnv.getTestProject();
         assertNotNull(project);
         assertTrue(project.isAccessible());
-        return project;
+        resources.add(project);
+        return resources;
     }
     
     /**
      * Basic sanity check that properties files can be loaded and contain
      * what's expected
      */
+    @SuppressWarnings("unchecked")
     public void testSanity()
     {
         try
@@ -106,6 +126,7 @@ public class TestResourceBundleMapSource extends TestCase
     /**
      * Verify the expected contents of bundle1 in project1
      */
+    @SuppressWarnings("unchecked")
     public void testContentsProject1Bundle1()
     {
         try
@@ -127,6 +148,7 @@ public class TestResourceBundleMapSource extends TestCase
     /**
      * Verify the expected contents of bundle1 in project2
      */
+    @SuppressWarnings("unchecked")
     public void testContentsProject2Bundle1()
     {
         try
@@ -145,4 +167,113 @@ public class TestResourceBundleMapSource extends TestCase
         }
     }
 
+    /**
+     * Regression test of https://bugs.eclipse.org/bugs/show_bug.cgi?id=196452.
+     * 
+     * This verifies that if the bundle file is deleted 
+     * then a reload on the bundle correctly cleans up the map
+     * @throws CoreException 
+     * @throws IOException 
+     * @throws JavaModelException 
+     */
+    public void testBundleDelete() throws JavaModelException, IOException, CoreException
+    {
+        // test the initial state, before outside meddling
+        Map  map = ResourceBundleMapSourceFactory.getResourceBundleMapSource(_project1, "bundles.bundle1");
+        assertTrue(map.containsKey("prop1"));
+        assertEquals("blah", map.get("prop1"));
+        assertTrue(map.containsKey("one.dot"));
+        assertEquals("blah1", map.get("one.dot"));
+        assertTrue(map.containsKey("two.dot.property"));
+        assertEquals("blah3", map.get("two.dot.property"));
+        
+        // now fiddle with the file
+        File bundleFile = _bundle1.getLocation().toFile();
+        assertTrue(bundleFile.delete());
+        _bundle1.refreshLocal(IResource.DEPTH_INFINITE, null);
+        
+        // the condition we are testing for is that the file is no
+        // longer accessible but the map tolerates this by becoming empty
+        assertFalse(_bundle1.isAccessible());
+        
+        // the map should now return empty and querying keys should
+        // return null
+        assertTrue(map.isEmpty());
+        assertNull(map.get("prop1"));
+        assertNull(map.get("one.dot"));
+        assertNull(map.get("two.dot.property"));
+    }
+
+    
+    
+    /**
+     * Regression test of https://bugs.eclipse.org/bugs/show_bug.cgi?id=196452.
+     * 
+     * This verifies that if the bundle file is modified outside of Eclipse, 
+     * then a reload on the bundle doesn't throw an out of sync exception.
+     * @throws CoreException 
+     * @throws IOException 
+     * @throws JavaModelException 
+     */
+//    public void testBundleSync() throws JavaModelException, IOException, CoreException
+//    {
+        // test the initial state, before outside meddling
+//        Map  map = ResourceBundleMapSourceFactory.getResourceBundleMapSource(_project1, "bundles.bundle1");
+//        assertTrue(map.containsKey("prop1"));
+//        assertEquals("blah", map.get("prop1"));
+//        assertTrue(map.containsKey("one.dot"));
+//        assertEquals("blah1", map.get("one.dot"));
+//        assertTrue(map.containsKey("two.dot.property"));
+//        assertEquals("blah3", map.get("two.dot.property"));
+//        
+//        // turn off automatic builds so the file we trying to
+//        // de-sync doesn't get auto-sync'd
+//        final boolean autoBuild = 
+//            _project1.getWorkspace().getDescription().isAutoBuilding();
+//        IWorkspaceDescription desc = 
+//            _project1.getWorkspace().getDescription();
+//        desc.setAutoBuilding(false);
+//        desc.set
+//        _project1.getWorkspace().setDescription(desc);
+//        
+//        // now fiddle with the file
+//        File bundleFile = _bundle1.getLocation().toFile();
+//        
+//        FileOutputStream outStream = null;
+//        
+//        try
+//        {
+//            outStream = new FileOutputStream(bundleFile,true);
+//            outStream.write("\nfiddleExternal=blah5".getBytes());
+//        }
+//        finally
+//        {
+//            if (outStream != null)
+//            {
+//                outStream.close();
+//            }
+//        }
+//        
+//        // the condition we are testing for is that the file is now out
+//        // of sync with the workspace but is still accessible
+//        assertFalse(_bundle1.isSynchronized(IResource.DEPTH_ZERO));
+//        assertTrue(_bundle1.isAccessible());
+//        
+//        // the map should now be updated to include the new key
+//        assertTrue(map.containsKey("fiddleExternal"));
+//        assertEquals("blah5", map.get("fiddleExternal"));
+//        
+//        // as well as the original ones (we appended to the file)
+//        assertTrue(map.containsKey("prop1"));
+//        assertEquals("blah", map.get("prop1"));
+//        assertTrue(map.containsKey("one.dot"));
+//        assertEquals("blah1", map.get("one.dot"));
+//        assertTrue(map.containsKey("two.dot.property"));
+//        assertEquals("blah3", map.get("two.dot.property"));
+//        
+//        // set auto-build back to it's initial state
+//        desc = _project1.getWorkspace().getDescription();
+//        desc.setAutoBuilding(autoBuild);
+//        _project1.getWorkspace().setDescription(desc);
+//    }
 }
