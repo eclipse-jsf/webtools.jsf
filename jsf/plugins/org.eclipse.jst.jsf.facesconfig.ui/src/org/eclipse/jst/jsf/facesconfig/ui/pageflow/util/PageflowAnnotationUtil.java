810c0d2bc514567581c42884b821bf6fe8b38014
cbateman <cbateman>
2008-01-21 23:48:45 +0000

    Removed discouraged access warning and ran a cleanup.

--- jsf/plugins/org.eclipse.jst.jsf.facesconfig.ui/src/org/eclipse/jst/jsf/facesconfig/ui/pageflow/util/PageflowAnnotationUtil.java
+++ jsf/plugins/org.eclipse.jst.jsf.facesconfig.ui/src/org/eclipse/jst/jsf/facesconfig/ui/pageflow/util/PageflowAnnotationUtil.java
@@ -34,135 +34,135 @@ import org.eclipse.jst.jsf.facesconfig.ui.pageflow.model.PageflowPage;
  */
 public class PageflowAnnotationUtil {
 
-	/**
-	 * 
-	 * validate the pageflow and its edit part
-	 * 
-	 * @param containerPart -
-	 *            it can be null, then the edit part will not be updated
-	 */
-	public static void validatePageflow(GraphicalEditPart containerPart) {
-		if (containerPart == null) {
-			return;
-		}
+    /**
+     * 
+     * validate the pageflow and its edit part
+     * 
+     * @param containerPart -
+     *            it can be null, then the edit part will not be updated
+     */
+    public static void validatePageflow(final GraphicalEditPart containerPart) {
+        if (containerPart == null) {
+            return;
+        }
 
-		// removeAllAnnotations(containerPart);
+        // removeAllAnnotations(containerPart);
 
-		// validate the nodes including page and actions.
-		Iterator iterChild = containerPart.getChildren().iterator();
-		while (iterChild.hasNext()) {
+        // validate the nodes including page and actions.
+        final Iterator iterChild = containerPart.getChildren().iterator();
+        while (iterChild.hasNext()) {
 
-			PageflowElementEditPart element = (PageflowElementEditPart) iterChild
-					.next();
-			// String errorMessage = null;
-			if (element instanceof PFValidator) {
-				((PFValidator) element).validate();
-			}
-			// if (element.getModel() instanceof PageflowPage) {
-			// errorMessage = PageflowValidation.getInstance().getNotifyMessage(
-			// (PageflowPage) element.getModel());
-			// }
+            final PageflowElementEditPart element = (PageflowElementEditPart) iterChild
+            .next();
+            // String errorMessage = null;
+            if (element instanceof PFValidator) {
+                ((PFValidator) element).validate();
+            }
+            // if (element.getModel() instanceof PageflowPage) {
+            // errorMessage = PageflowValidation.getInstance().getNotifyMessage(
+            // (PageflowPage) element.getModel());
+            // }
 
-			// if (errorMessage != null) {
-			// addAnnotation(element, errorMessage);
-			// } else {
-			// removeAnnotation(element);
-			// }
+            // if (errorMessage != null) {
+            // addAnnotation(element, errorMessage);
+            // } else {
+            // removeAnnotation(element);
+            // }
 
-			// validate the connections.
-			Iterator iterLinks = element.getSourceConnections().iterator();
-			while (iterLinks.hasNext()) {
-				PageflowLinkEditPart link = (PageflowLinkEditPart) iterLinks
-						.next();
-				link.validate();
-				// validateLink(link);
-			}
-		}
+            // validate the connections.
+            final Iterator iterLinks = element.getSourceConnections().iterator();
+            while (iterLinks.hasNext()) {
+                final PageflowLinkEditPart link = (PageflowLinkEditPart) iterLinks
+                .next();
+                link.validate();
+                // validateLink(link);
+            }
+        }
 
-	}
+    }
 
-	/**
-	 * validate the page based on model and its's edit part.
-	 * 
-	 * @param pagePart -
-	 *            it can be null, then the edit part will not be updated
-	 */
-	public static void validatePage(PageflowNodeEditPart pagePart) {
-		if (pagePart == null && pagePart.getParent() != null) {
-			return;
-		}
+    /**
+     * validate the page based on model and its's edit part.
+     * 
+     * @param pagePart -
+     *            it can be null, then the edit part will not be updated
+     */
+    public static void validatePage(final PageflowNodeEditPart pagePart) {
+        if (pagePart != null && pagePart.getParent() != null) {
+            return;
+        }
 
-		String errorMessage = null;
+        String errorMessage = null;
 
-		removeAnnotation(pagePart);
+        removeAnnotation(pagePart);
 
-		errorMessage = PageflowValidation.getInstance().getNotifyMessage(
-				(PageflowPage) pagePart.getModel());
+        errorMessage = PageflowValidation.getInstance().getNotifyMessage(
+                (PageflowPage) pagePart.getModel());
 
-		if (errorMessage != null) {
-			addAnnotation(pagePart, errorMessage);
-		} else {
-			removeAnnotation(pagePart);
-		}
+        if (errorMessage != null) {
+            addAnnotation(pagePart, errorMessage);
+        } else {
+            removeAnnotation(pagePart);
+        }
 
-		// validate the connections.
-		// Iterator iterLinks = pagePart.getSourceConnections().iterator();
-		// while (iterLinks.hasNext()) {
-		// PageflowLinkEditPart link = (PageflowLinkEditPart) iterLinks.next();
-		//
-		// validateLink(link);
-		// }
-	}
+        // validate the connections.
+        // Iterator iterLinks = pagePart.getSourceConnections().iterator();
+        // while (iterLinks.hasNext()) {
+        // PageflowLinkEditPart link = (PageflowLinkEditPart) iterLinks.next();
+        //
+        // validateLink(link);
+        // }
+    }
 
-	/**
-	 * validate the link based on model and its's edit part.
-	 * 
-	 * @param linkPart -
-	 *            it can be null, then the edit part will not be updated
-	 */
-	public static void validateLink(PageflowLinkEditPart linkPart) {
-		if (linkPart != null && linkPart.getParent() != null) {
-			String errorMessage = null;
+    /**
+     * validate the link based on model and its's edit part.
+     * 
+     * @param linkPart -
+     *            it can be null, then the edit part will not be updated
+     */
+    public static void validateLink(final PageflowLinkEditPart linkPart) {
+        if (linkPart != null && linkPart.getParent() != null) {
+            String errorMessage = null;
 
-			removeAnnotation(linkPart);
+            removeAnnotation(linkPart);
 
-			errorMessage = PageflowValidation.getInstance().isValidLink(
-					(PageflowLink) linkPart.getModel());
+            errorMessage = PageflowValidation.getInstance().isValidLink(
+                    (PageflowLink) linkPart.getModel());
 
-			if (errorMessage != null) {
-				addAnnotation(linkPart, errorMessage);
-			} else {
-				removeAnnotation(linkPart);
-			}
-		}
-	}
+            if (errorMessage != null) {
+                addAnnotation(linkPart, errorMessage);
+            } else {
+                removeAnnotation(linkPart);
+            }
+        }
+    }
 
-	/**
-	 * add a marker with IMarker.PROBLEM type, IMarker.SEVERITY_ERROR severity,
-	 * and message.
-	 * 
-	 * @param editPart
-	 * @param model
-	 * @param message
-	 */
-	private static void addAnnotation(GraphicalEditPart editPart, String message) {
-		if (editPart != null && editPart instanceof IAnnotationEditPart) {
-			Annotation annotation = new Annotation(IMarker.PROBLEM, false,
-					message);
-			((IAnnotationEditPart) editPart).addAnnotation(annotation);
-		}
-	}
+    /**
+     * add a marker with IMarker.PROBLEM type, IMarker.SEVERITY_ERROR severity,
+     * and message.
+     * 
+     * @param editPart
+     * @param model
+     * @param message
+     */
+    private static void addAnnotation(final GraphicalEditPart editPart, final String message) {
+        if (editPart != null && editPart instanceof IAnnotationEditPart) {
+            final Annotation annotation = new Annotation(IMarker.PROBLEM, false,
+                    message);
+            ((IAnnotationEditPart) editPart).addAnnotation(annotation);
+        }
+    }
 
-	/**
-	 * remove a marker
-	 * 
-	 * @param editPart
-	 * @param model
-	 */
-	private static void removeAnnotation(GraphicalEditPart editPart) {
-		if (editPart != null && editPart instanceof IAnnotationEditPart) {
-			((IAnnotationEditPart) editPart).removeAnnotation();
-		}
-	}
+    /**
+     * remove a marker
+     * 
+     * @param editPart
+     * @param model
+     */
+    private static void removeAnnotation(final GraphicalEditPart editPart) {
+        if (editPart != null && editPart instanceof IAnnotationEditPart) {
+            ((IAnnotationEditPart) editPart).removeAnnotation();
+        }
+    }
 
 }
--- jsf/plugins/org.eclipse.jst.jsf.facesconfig.ui/src/org/eclipse/jst/jsf/facesconfig/ui/preference/LinkFigure.java
+++ jsf/plugins/org.eclipse.jst.jsf.facesconfig.ui/src/org/eclipse/jst/jsf/facesconfig/ui/preference/LinkFigure.java
@@ -34,281 +34,303 @@ import org.eclipse.swt.graphics.Image;
  * @author Bob
  */
 /*package*/ class LinkFigure extends PolylineConnection implements IBaseFigure {
-	private final static Color defaultLineColor = ColorConstants.black;
+    private final static Color defaultLineColor = ColorConstants.black;
 
-	private final static int defaultLineWidth = 1;
+    private final static int defaultLineWidth = 1;
 
-	private final static boolean defaultLabelVisible = false;
+    private final static boolean defaultLabelVisible = false;
 
-	private final static Color defaultLabelForeground = ColorConstants.black;
+    private final static Color defaultLabelForeground = ColorConstants.black;
 
-	private final static Color defaultLabelBackground = ColorConstants.white;
+    private final static Color defaultLabelBackground = ColorConstants.white;
 
-	private final static Font defaultFont = JFaceResources.getFontRegistry().get(
-			JFaceResources.DEFAULT_FONT);
+    private final static Font defaultFont = JFaceResources.getFontRegistry().get(
+            JFaceResources.DEFAULT_FONT);
 
-	private LinkLabel label;
+    private LinkLabel label;
 
-	/**
-	 * Default constructor
-	 */
-	public LinkFigure() {
-		super();
-		setLineWidth(defaultLineWidth);
-		setForegroundColor(defaultLineColor);
+    /**
+     * Default constructor
+     */
+    public LinkFigure() {
+        super();
+        setLineWidth(defaultLineWidth);
+        setForegroundColor(defaultLineColor);
 
-		// BaseFigureDecorator decorator;
-		// decorator = new
-		// BaseFigureDecorator(GEMPlugin.getDefault().getGEMImage("delete.gif"),"target",PositionConstants.WEST);
-		// addDecorator(decorator);
-		// decorator = new
-		// BaseFigureDecorator(GEMPlugin.getDefault().getGEMImage("delete.gif"),"target",PositionConstants.EAST);
-		// addDecorator(decorator);
-	}
+        // BaseFigureDecorator decorator;
+        // decorator = new
+        // BaseFigureDecorator(GEMPlugin.getDefault().getGEMImage("delete.gif"),"target",PositionConstants.WEST);
+        // addDecorator(decorator);
+        // decorator = new
+        // BaseFigureDecorator(GEMPlugin.getDefault().getGEMImage("delete.gif"),"target",PositionConstants.EAST);
+        // addDecorator(decorator);
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.sybase.stf.gem.diagram.editor.figures.IBaseFigure#setToolTipText()
-	 */
-	public void setToolTipText(String text) {
-		Label toolTip_ = null;
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.sybase.stf.gem.diagram.editor.figures.IBaseFigure#setToolTipText()
+     */
+    public void setToolTipText(final String text) {
+        Label toolTip_ = null;
 
-		if (text != null && text.length() > 0) {
-			toolTip_ = new Label(text);
-			toolTip_.setBorder(new MarginBorder(3));
-		}
+        if (text != null && text.length() > 0) {
+            toolTip_ = new Label(text);
+            toolTip_.setBorder(new MarginBorder(3));
+        }
 
-		super.setToolTip(toolTip_);
-	}
+        super.setToolTip(toolTip_);
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.sybase.stf.gem.diagram.editor.figures.IBaseFigure#getToolTipText()
-	 */
-	public String getToolTipText() {
-		if (getToolTip() != null)
-			return ((Label) getToolTip()).getText();
-		return null;
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.sybase.stf.gem.diagram.editor.figures.IBaseFigure#getToolTipText()
+     */
+    public String getToolTipText() {
+        if (getToolTip() != null)
+        {
+            return ((Label) getToolTip()).getText();
+        }
+        return null;
+    }
 
-	public void setHighlight(boolean flag) {
-		if (flag) {
-			setForegroundColor(ColorConstants.titleBackground);
-			// setLineWidth(defaultLineWidth * 2);
-		} else {
-			setForegroundColor(defaultLineColor);
-			// setLineWidth(defaultLineWidth);
-		}
-	}
+    public void setHighlight(final boolean flag) {
+        if (flag) {
+            setForegroundColor(ColorConstants.titleBackground);
+            // setLineWidth(defaultLineWidth * 2);
+        } else {
+            setForegroundColor(defaultLineColor);
+            // setLineWidth(defaultLineWidth);
+        }
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.sybase.stf.gem.diagram.editor.figures.IBaseFigure#setText()
-	 */
-	public void setText(String text) {
-		if (text == null && text.length() == 0) {
-			if (label != null) {
-				remove(label);
-				label = null;
-			}
-		} else {
-			getLabel().setText(text);
-		}
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.sybase.stf.gem.diagram.editor.figures.IBaseFigure#setText()
+     */
+    public void setText(final String text) {
+        if (text != null && text.length() == 0) {
+            if (label != null) {
+                remove(label);
+                label = null;
+            }
+        } else {
+            getLabel().setText(text);
+        }
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.sybase.stf.gem.diagram.editor.figures.IBaseFigure#getText()
-	 */
-	public String getText() {
-		if (label == null)
-			return null;
-		return label.getText();
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.sybase.stf.gem.diagram.editor.figures.IBaseFigure#getText()
+     */
+    public String getText() {
+        if (label == null)
+        {
+            return null;
+        }
+        return label.getText();
+    }
 
-	/*
-	 * CR374981: Long activity labels do not get wrapped or truncated This
-	 * method was added to the IBaseFigure interface to support direct edit of
-	 * figure labels on the canvas.
-	 */
-	public Rectangle getTextBounds() {
-		return label.getTextBounds();
-	}
+    /*
+     * CR374981: Long activity labels do not get wrapped or truncated This
+     * method was added to the IBaseFigure interface to support direct edit of
+     * figure labels on the canvas.
+     */
+    public Rectangle getTextBounds() {
+        return label.getTextBounds();
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.sybase.stf.gem.diagram.editor.figures.IBaseFigure#setIcon(org.eclipse.swt.graphics.Image)
-	 */
-	public void setIcon(Image image) {
-		getLabel().setIcon(image);
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.sybase.stf.gem.diagram.editor.figures.IBaseFigure#setIcon(org.eclipse.swt.graphics.Image)
+     */
+    public void setIcon(final Image image) {
+        getLabel().setIcon(image);
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.sybase.stf.gem.diagram.editor.figures.IBaseFigure#getIcon()
-	 */
-	public Image getIcon() {
-		return getLabel().getIcon();
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.sybase.stf.gem.diagram.editor.figures.IBaseFigure#getIcon()
+     */
+    public Image getIcon() {
+        return getLabel().getIcon();
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.sybase.stf.gem.diagram.editor.figures.IBaseFigure#addDecorator(com.sybase.stf.gem.diagram.editor.figures.BaseFigureDecorator)
-	 */
-	public void addDecorator(BaseFigureDecorator decorator) {
-		int pos = decorator.getPosition();
-		if (pos == BaseFigureDecorator.DEFAULT_DECORATOR_POSITION
-				|| pos == PositionConstants.WEST
-				|| pos == PositionConstants.LEFT) {
-			setTargetDecoration(decorator);
-		} else if (pos == PositionConstants.EAST
-				|| pos == PositionConstants.RIGHT) {
-			setSourceDecoration(decorator);
-		}
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.sybase.stf.gem.diagram.editor.figures.IBaseFigure#addDecorator(com.sybase.stf.gem.diagram.editor.figures.BaseFigureDecorator)
+     */
+    public void addDecorator(final BaseFigureDecorator decorator) {
+        final int pos = decorator.getPosition();
+        if (pos == BaseFigureDecorator.DEFAULT_DECORATOR_POSITION
+                || pos == PositionConstants.WEST
+                || pos == PositionConstants.LEFT) {
+            setTargetDecoration(decorator);
+        } else if (pos == PositionConstants.EAST
+                || pos == PositionConstants.RIGHT) {
+            setSourceDecoration(decorator);
+        }
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.sybase.stf.gem.diagram.editor.figures.IBaseFigure#removeDecorator()
-	 */
-	public void removeDecorator() {
-		removeDecorator(BaseFigureDecorator.DEFAULT_DECORATOR_POSITION);
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.sybase.stf.gem.diagram.editor.figures.IBaseFigure#removeDecorator()
+     */
+    public void removeDecorator() {
+        removeDecorator(BaseFigureDecorator.DEFAULT_DECORATOR_POSITION);
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.sybase.stf.gem.diagram.editor.figures.IBaseFigure#removeDecorator(int)
-	 */
-	public void removeDecorator(int position) {
-		if (position == BaseFigureDecorator.DEFAULT_DECORATOR_POSITION)
-			setTargetDecoration(null);
-		else
-			setSourceDecoration(null);
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.sybase.stf.gem.diagram.editor.figures.IBaseFigure#removeDecorator(int)
+     */
+    public void removeDecorator(final int position) {
+        if (position == BaseFigureDecorator.DEFAULT_DECORATOR_POSITION)
+        {
+            setTargetDecoration(null);
+        }
+        else
+        {
+            setSourceDecoration(null);
+        }
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.sybase.stf.gem.diagram.editor.figures.IBaseFigure#getDecorators()
-	 */
-	public List getDecorators() {
-		List list = new ArrayList(2);
-		Object d;
-		d = getSourceDecoration();
-		if (d != null)
-			list.add(d);
-		d = getTargetDecoration();
-		if (d != null)
-			list.add(d);
-		return list;
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.sybase.stf.gem.diagram.editor.figures.IBaseFigure#getDecorators()
+     */
+    public List getDecorators() {
+        final List list = new ArrayList(2);
+        Object d;
+        d = getSourceDecoration();
+        if (d != null)
+        {
+            list.add(d);
+        }
+        d = getTargetDecoration();
+        if (d != null)
+        {
+            list.add(d);
+        }
+        return list;
+    }
 
-	private LinkLabel getLabel() {
-		if (label == null) {
-			label = new LinkLabel();
-			add(label, new MidpointLocator(this, 0));
-		}
-		return label;
-	}
+    private LinkLabel getLabel() {
+        if (label == null) {
+            label = new LinkLabel();
+            add(label, new MidpointLocator(this, 0));
+        }
+        return label;
+    }
 
-	public void setFont(Font f) {
-		getLabel().setFont(f);
-	}
+    @Override
+    public void setFont(final Font f) {
+        getLabel().setFont(f);
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.draw2d.Shape#setLineWidth(int)
-	 */
-	public void setLineWidth(int w) {
-		getLabel().setBorderWidth(w);
-		super.setLineWidth(w);
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.draw2d.Shape#setLineWidth(int)
+     */
+    @Override
+    public void setLineWidth(final int w) {
+        getLabel().setBorderWidth(w);
+        super.setLineWidth(w);
+    }
 
-	public void setForegroundColor(Color c) {
-		getLabel().setBorderColor(c);
-		super.setForegroundColor(c);
-	}
+    @Override
+    public void setForegroundColor(final Color c) {
+        getLabel().setBorderColor(c);
+        super.setForegroundColor(c);
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.draw2d.Polyline#setPoints(org.eclipse.draw2d.geometry.PointList)
-	 */
-	public void setPoints(PointList points) {
-		super.setPoints(points);
-		if (label != null) {
-			int i = getPoints().size() / 2 - 1;
-			if (i < 0)
-				i = 0;
-			if (getLayoutManager() != null)
-				getLayoutManager().setConstraint(label,
-						new MidpointLocator(this, i));
-		}
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.draw2d.Polyline#setPoints(org.eclipse.draw2d.geometry.PointList)
+     */
+    @Override
+    public void setPoints(final PointList points) {
+        super.setPoints(points);
+        if (label != null) {
+            int i = getPoints().size() / 2 - 1;
+            if (i < 0)
+            {
+                i = 0;
+            }
+            if (getLayoutManager() != null)
+            {
+                getLayoutManager().setConstraint(label,
+                        new MidpointLocator(this, i));
+            }
+        }
+    }
 
-	public Rectangle getBounds() {
-		if (bounds == null) {
-			if (bounds == null) {
-				bounds = getPoints().getBounds().getExpanded(lineWidth / 2,
-						lineWidth / 2);
-			}
-			// CR382243: Clicking on the node icon selects the connection line
-			// rather than the node
-			// NOTE: expanding the bounds to include children (in this case,
-			// the Link labels) causes problems when the connection line becomes
-			// short (i.e., when the 2 anchor figures at each end of the
-			// connection are
-			// close together) because the bounds of this link figure will be
-			// greater
-			// than the actual length of the line. This causes the link figure
-			// to
-			// be hit-tested beyond its visual size.
-			if (getLabel().isVisible()) {
-				for (int i = 0; i < getChildren().size(); i++) {
-					IFigure child = (IFigure) getChildren().get(i);
-					bounds.union(child.getBounds());
-				}
-			}
-		}
-		return bounds;
-	}
+    @Override
+    public Rectangle getBounds() {
+        if (bounds == null) {
+            if (bounds == null) {
+                bounds = getPoints().getBounds().getExpanded(lineWidth / 2,
+                        lineWidth / 2);
+            }
+            // CR382243: Clicking on the node icon selects the connection line
+            // rather than the node
+            // NOTE: expanding the bounds to include children (in this case,
+            // the Link labels) causes problems when the connection line becomes
+            // short (i.e., when the 2 anchor figures at each end of the
+            // connection are
+            // close together) because the bounds of this link figure will be
+            // greater
+            // than the actual length of the line. This causes the link figure
+            // to
+            // be hit-tested beyond its visual size.
+            if (getLabel().isVisible()) {
+                for (int i = 0; i < getChildren().size(); i++) {
+                    final IFigure child = (IFigure) getChildren().get(i);
+                    bounds.union(child.getBounds());
+                }
+            }
+        }
+        return bounds;
+    }
 
-	/*
-	 * Helper class for line labels.
-	 */
-	private static class LinkLabel extends Label {
-		LinkLabel() {
-			setBorder(new LineBorder());
-			setOpaque(true);
-			setForegroundColor(defaultLabelForeground);
-			setBackgroundColor(defaultLabelBackground);
-			setFont(defaultFont);
-			LineBorder border_ = new LineBorder(defaultLineWidth);
-			setBorder(border_);
-			border_.setColor(defaultLineColor);
-			setVisible(defaultLabelVisible);
-		}
+    /*
+     * Helper class for line labels.
+     */
+    private static class LinkLabel extends Label {
+        LinkLabel() {
+            setBorder(new LineBorder());
+            setOpaque(true);
+            setForegroundColor(defaultLabelForeground);
+            setBackgroundColor(defaultLabelBackground);
+            setFont(defaultFont);
+            final LineBorder border_ = new LineBorder(defaultLineWidth);
+            setBorder(border_);
+            border_.setColor(defaultLineColor);
+            setVisible(defaultLabelVisible);
+        }
 
-		void setBorderWidth(int w) {
-			((LineBorder) getBorder()).setWidth(w);
-		}
+        void setBorderWidth(final int w) {
+            ((LineBorder) getBorder()).setWidth(w);
+        }
 
-		void setBorderColor(Color c) {
-			((LineBorder) getBorder()).setColor(c);
-		}
+        void setBorderColor(final Color c) {
+            ((LineBorder) getBorder()).setColor(c);
+        }
 
-		protected boolean useLocalCoordinates() {
-			return false;
-		}
-	}
+        @Override
+        protected boolean useLocalCoordinates() {
+            return false;
+        }
+    }
 }
