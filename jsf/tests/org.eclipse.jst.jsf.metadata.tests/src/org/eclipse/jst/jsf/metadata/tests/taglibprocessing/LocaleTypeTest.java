8fb93bf465e858b5fce9cc9a4275e30e85d89ce5
gkessler <gkessler>
2008-03-26 18:44:41 +0000

    [191827] Provide developers with a way to create EnumerationType's that resolve to multiple signatures.

--- jsf/tests/org.eclipse.jst.jsf.metadata.tests/src/org/eclipse/jst/jsf/metadata/tests/taglibprocessing/LocaleTypeTest.java
+++ jsf/tests/org.eclipse.jst.jsf.metadata.tests/src/org/eclipse/jst/jsf/metadata/tests/taglibprocessing/LocaleTypeTest.java
@@ -1,11 +1,29 @@
+/*******************************************************************************
+ * Copyright (c) 2008 Oracle Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Oracle - initial API and implementation
+ *    
+ ********************************************************************************/
+
 package org.eclipse.jst.jsf.metadata.tests.taglibprocessing;
 
 import junit.framework.Assert;
 
+import org.eclipse.jst.jsf.metadataprocessors.features.ELIsNotValidException;
 import org.eclipse.jst.jsf.metadataprocessors.features.IPossibleValues;
+import org.eclipse.jst.jsf.metadataprocessors.features.IValidELValues;
 import org.eclipse.jst.jsf.metadataprocessors.features.IValidValues;
+import org.eclipse.jst.jsf.taglibprocessing.attributevalues.MultiSignatureEnumerationType;
 import org.eclipse.jst.pagedesigner.editors.properties.IPropertyPageDescriptor;
 
+/**
+ * Tests {@link LocaleTypeTest} and {@link MultiSignatureEnumerationType} *
+ */
 public class LocaleTypeTest extends TaglibProcessingTestCase {
 	public void testPossibleValues(){		
 		Assert.assertNotNull(possibleValueAdapters);
@@ -30,9 +48,25 @@ public class LocaleTypeTest extends TaglibProcessingTestCase {
 		Assert.assertFalse(propertyPageDescriptorAdapters.isEmpty());
 		
 		IPropertyPageDescriptor ppd =(IPropertyPageDescriptor)propertyPageDescriptorAdapters.get(0);
-		//positive tests
+		//positive test
 		assertEquals("ACategory", ppd.getCategory());
 		assertEquals(false, ppd.isRequired());
 
+	}
+	
+	public void testELValidValues() {
+		//value can be String (non-EL) or an EL expression pointing at Locale
+		Assert.assertNotNull(validELValuesAdapters);
+		Assert.assertFalse(validELValuesAdapters.isEmpty());
+		
+		IValidELValues vv =(IValidELValues)validELValuesAdapters.get(0);
+		try {
+			String[] validSigs = vv.getExpectedRuntimeType().getSignatures();
+			assertEquals(2, validSigs.length);			
+			assertEquals("Ljava.util.Locale;", validSigs[0]);
+			assertEquals("Ljava.lang.String;", validSigs[1]);
+		} catch (ELIsNotValidException e) {		
+			e.printStackTrace();
+		}
 	}
 }
