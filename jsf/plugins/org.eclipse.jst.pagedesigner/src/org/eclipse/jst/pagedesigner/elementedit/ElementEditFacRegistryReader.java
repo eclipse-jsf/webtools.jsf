e4102ec9ba31423efbcd2c5e881d3c3ad4a1ae83
cbateman <cbateman>
2009-01-28 06:23:03 +0000

    Backport partial fix for  https://bugs.eclipse.org/bugs/show_bug.cgi?id=250850.

--- jsf/plugins/org.eclipse.jst.pagedesigner/src/org/eclipse/jst/pagedesigner/elementedit/ElementEditFacRegistryReader.java
+++ jsf/plugins/org.eclipse.jst.pagedesigner/src/org/eclipse/jst/pagedesigner/elementedit/ElementEditFacRegistryReader.java
@@ -22,6 +22,7 @@ import org.eclipse.core.runtime.IExtensionPoint;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.jst.pagedesigner.IJMTConstants;
 import org.eclipse.jst.pagedesigner.PDPlugin;
+import org.eclipse.jst.pagedesigner.PageDesignerTraceOptions;
 
 /**
  * @author mengbo
@@ -41,38 +42,79 @@ public class ElementEditFacRegistryReader {
 		return Collections.unmodifiableList(_handlers);
 	}
 
-	private static List<IElementEditFactory> readAllHandlers() {
-		List result = new ArrayList();
-		IExtensionPoint extensionPoint = Platform.getExtensionRegistry()
-				.getExtensionPoint(PDPlugin.getPluginId(),
-						IJMTConstants.EXTENSION_POINT_PAGEDESIGNER);
-		IExtension[] extensions = extensionPoint.getExtensions();
+	private static List<IElementEditFactory> readAllHandlers()
+    {
+        List<IElementEditFactory> result = new ArrayList<IElementEditFactory>();
+        IExtensionPoint extensionPoint = Platform.getExtensionRegistry()
+                .getExtensionPoint(PDPlugin.getPluginId(),
+                        IJMTConstants.EXTENSION_POINT_PAGEDESIGNER);
+        IExtension[] extensions = extensionPoint.getExtensions();
 
-		for (int i = 0; i < extensions.length; i++) {
-			IExtension ext = extensions[i];
-			IConfigurationElement[] dropHandlers = ext
-					.getConfigurationElements();
+        for (int i = 0; i < extensions.length; i++)
+        {
+            IExtension ext = extensions[i];
+            IConfigurationElement[] elementEditElement = ext
+                    .getConfigurationElements();
 
-			for (int j = 0; j < dropHandlers.length; j++) {
-				if (dropHandlers[j].getName().equals(
-						IJMTConstants.ELEMENT_EDIT_FACTORY)) {
-					dropHandlers[j].getAttribute("class");
-					Object obj;
-					try {
-						obj = dropHandlers[j]
-								.createExecutableExtension("class");
+            for (int j = 0; j < elementEditElement.length; j++)
+            {
+                final IConfigurationElement element = elementEditElement[j];
+                if (element.getName().equals(
+                        IJMTConstants.ELEMENT_EDIT_FACTORY))
+                {
+                    elementEditElement[j].getAttribute("class"); //$NON-NLS-1$
+                    Object obj;
+                    try
+                    {
+                        obj = elementEditElement[j]
+                                .createExecutableExtension("class"); //$NON-NLS-1$
 
-						if (obj instanceof IElementEditFactory) {
-							result.add(obj);
-						}
-					} catch (CoreException e) {
-						// ignore the exception
-						e.printStackTrace();
-					}
-				}
-			}
-		}
-		return result;
-	}
+                        // TODO: we need a policy based solution here,
+                        // but this will do for now
+                        if (obj instanceof IElementEditFactory)
+                        {
+                            if (element.getContributor().getName().startsWith("org.eclipse.jst")) //$NON-NLS-1$
+                            {
+                                if (PageDesignerTraceOptions.TRACE_ELEMENTEDITLOAD)
+                                {
+                                    PageDesignerTraceOptions.log("ElementEditFacRegistryReader: Appending to list:"+obj.getClass().getName()); //$NON-NLS-1$
+                                }
+                                // push JSF tools provided ones to the end
+                                result.add((IElementEditFactory) obj);
+                            }
+                            // prepend if something outside JSF tools declared it
+                            else
+                            {
+                                if (PageDesignerTraceOptions.TRACE_ELEMENTEDITLOAD)
+                                {
+                                    PageDesignerTraceOptions.log("ElementEditFacRegistryReader: Prepending to list:"+obj.getClass().getName()); //$NON-NLS-1$
+                                }
+                                // this way, adopters can put their overrides
+                                // of factories with built-in support like
+                                // JSF HTML/CORE will be used first
+                                result.add(0, (IElementEditFactory) obj);
+                            }
+                        }
+                    } catch (CoreException e)
+                    {
+                        PDPlugin.log("Problem loading element edit extension for "+element.toString(), e); //$NON-NLS-1$
+                    }
+                }
+            }
+        }
+        
+
+        if (PageDesignerTraceOptions.TRACE_ELEMENTEDITLOAD)
+        {
+            PageDesignerTraceOptions.log("\nFinal elementEditFactory list in order:"); //$NON-NLS-1$
+            for (final IElementEditFactory factory : result)
+            {
+                PageDesignerTraceOptions.log(factory.getClass().getName());
+            }
+            PageDesignerTraceOptions.log("\n"); //$NON-NLS-1$
+        }
+
+        return result;
+    }
 
 }
--- jsf/plugins/org.eclipse.jst.pagedesigner/src/org/eclipse/jst/pagedesigner/elementedit/ElementEditFactoryRegistry.java
+++ jsf/plugins/org.eclipse.jst.pagedesigner/src/org/eclipse/jst/pagedesigner/elementedit/ElementEditFactoryRegistry.java
@@ -16,6 +16,7 @@ import java.util.List;
 
 import org.eclipse.jst.jsf.common.dom.TagIdentifier;
 import org.eclipse.jst.jsf.core.internal.tld.TagIdentifierFactory;
+import org.eclipse.jst.pagedesigner.PageDesignerTraceOptions;
 import org.eclipse.jst.pagedesigner.elementedit.html.HTMLElementEditFactory;
 import org.eclipse.jst.pagedesigner.elementedit.jsp.JSPElementEditFactory;
 import org.w3c.dom.Element;
@@ -24,93 +25,122 @@ import org.w3c.dom.Element;
  * @author mengbo
  * @version 1.5
  */
-public class ElementEditFactoryRegistry {
-	List _factories = new ArrayList();
+public class ElementEditFactoryRegistry
+{
+    List _factories = new ArrayList();
 
-	private static ElementEditFactoryRegistry _instance;
-
-	/**
-	 * 
-	 */
-	private ElementEditFactoryRegistry() {
-		addFactory(new HTMLElementEditFactory());
-        addFactory(new JSPElementEditFactory());
-
-		List<IElementEditFactory> facs = 
-		    ElementEditFacRegistryReader.getAllHandlers();
-
-		if (facs != null) 
-		{
-			for (IElementEditFactory fac : facs) 
-			{
-				addFactory(fac);
-			}
-		}
-	}
-
-	/**
-	 * Add a factory to list available.
-	 * 
-	 * @param fac
-	 */
-	public void addFactory(IElementEditFactory fac) {
-		_factories.add(fac);
-	}
+    private static ElementEditFactoryRegistry _instance;
 
     /**
-     * @param tagIdentifier 
-     * @return an IElementEdit constructed for the tag uniquely identified
-     * by the ns uri (tag uri for JSP tags) and tagName (element name) or null
-     * if the system can't create one.
+	 *
+	 */
+    private ElementEditFactoryRegistry()
+    {
+        final List<IElementEditFactory> facs = ElementEditFacRegistryReader
+                .getAllHandlers();
+
+        if (facs != null)
+        {
+            for (final IElementEditFactory fac : facs)
+            {
+                addFactory(fac);
+            }
+        }
+        addFactory(new HTMLElementEditFactory());
+        addFactory(new JSPElementEditFactory());
+    }
+
+    /**
+     * Add a factory to list available.
+     *
+     * @param fac
+     */
+    public void addFactory(final IElementEditFactory fac)
+    {
+        _factories.add(fac);
+    }
+
+    /**
+     * @param tagIdentifier
+     * @return an IElementEdit constructed for the tag uniquely identified by
+     *         the ns uri (tag uri for JSP tags) and tagName (element name) or
+     *         null if the system can't create one.
      */
     public IElementEdit createElementEdit(final TagIdentifier tagIdentifier)
     {
         final String uri = tagIdentifier.getUri();
-        
+
         // first round, match uri
-        for (int i = 0, size = _factories.size(); i < size; i++) {
-            IElementEditFactory fac = (IElementEditFactory) _factories.get(i);
-            String facuri = fac.getSupportedURI();
-            if (facuri != null && facuri.equals(uri)) {
-                IElementEdit elementEdit = fac.createElementEdit(tagIdentifier);
-                if (elementEdit != null) {
+        for (int i = 0, size = _factories.size(); i < size; i++)
+        {
+            final IElementEditFactory fac = (IElementEditFactory) _factories.get(i);
+            final String facuri = fac.getSupportedURI();
+            if (facuri != null && facuri.equals(uri))
+            {
+                final IElementEdit elementEdit = fac.createElementEdit(tagIdentifier);
+                if (elementEdit != null)
+                {
+                    if (PageDesignerTraceOptions.TRACE_ELEMENTEDITSELECTION)
+                    {
+                        PageDesignerTraceOptions.log("ElementEditFactoryRegistry: first loop, "+ //$NON-NLS-1$
+                                String.format("Selected %s for uri=%s, tagname=%s", elementEdit.getClass().getName(),uri, tagIdentifier.getTagName())); //$NON-NLS-1$
+                    }
                     return elementEdit;
                 }
             }
         }
         // second round
-        for (int i = 0, size = _factories.size(); i < size; i++) {
-            IElementEditFactory fac = (IElementEditFactory) _factories.get(i);
-            String facuri = fac.getSupportedURI();
-            if (facuri == null) {
-                IElementEdit elementEdit = fac.createElementEdit(tagIdentifier);
-                if (elementEdit != null) {
+        for (int i = 0, size = _factories.size(); i < size; i++)
+        {
+            final IElementEditFactory fac = (IElementEditFactory) _factories.get(i);
+            final String facuri = fac.getSupportedURI();
+            if (facuri == null)
+            {
+                final IElementEdit elementEdit = fac.createElementEdit(tagIdentifier);
+                if (elementEdit != null)
+                {
+                    if (PageDesignerTraceOptions.TRACE_ELEMENTEDITSELECTION)
+                    {
+                        PageDesignerTraceOptions.log("ElementEditFactoryRegistry: second loop, "+ //$NON-NLS-1$
+                                String.format("Selected %s for uri=%s, tagname=%s", elementEdit.getClass().getName(),uri, tagIdentifier.getTagName())); //$NON-NLS-1$
+                    }
                     return elementEdit;
                 }
             }
         }
+
+        if (PageDesignerTraceOptions.TRACE_ELEMENTEDITSELECTION)
+        {
+            PageDesignerTraceOptions.log("ElementEditFactoryRegistry: no element edit found, returning null for "+ //$NON-NLS-1$
+                    String.format("uri=%s, name=%s", tagIdentifier.getUri(), tagIdentifier.getTagName())); //$NON-NLS-1$
+        }
+
         return null;
     }
-    
-	/**
-     * Convenience method for createElementEdit(uri, tagName) that takes
-     * a tag element.
-     * 
-	 * @param ele
-	 * @return an element edit
-	 */
-	public IElementEdit createElementEdit(Element ele) {
-		final TagIdentifier tagIdentifier = TagIdentifierFactory.createDocumentTagWrapper(ele);
-        return createElementEdit(tagIdentifier);
-	}
 
-	/**
-	 * @return the single instance of the factory registry
-	 */
-	public static ElementEditFactoryRegistry getInstance() {
-		if (_instance == null) {
-			_instance = new ElementEditFactoryRegistry();
-		}
-		return _instance;
-	}
+    /**
+     * Convenience method for createElementEdit(uri, tagName) that takes a tag
+     * element.
+     *
+     * @param ele
+     * @return an element edit
+     */
+    public IElementEdit createElementEdit(final Element ele)
+    {
+        final TagIdentifier tagIdentifier = TagIdentifierFactory
+                .createDocumentTagWrapper(ele);
+        return createElementEdit(tagIdentifier);
+    }
+
+    /**
+     * @return the single instance of the factory registry
+     */
+    public synchronized static ElementEditFactoryRegistry getInstance()
+    {
+        if (_instance == null)
+        {
+            _instance = new ElementEditFactoryRegistry();
+        }
+        return _instance;
+    }
 }
