009e99673820097e4f8ca31332dd816aee6cba35
cbateman <cbateman>
2009-05-05 23:00:29 +0000

    Missed file in fix for https://bugs.eclipse.org/bugs/show_bug.cgi?id=267967.

--- jsf/plugins/org.eclipse.jst.jsf.common/src/org/eclipse/jst/jsf/context/AbstractDelegatingFactory.java
+++ jsf/plugins/org.eclipse.jst.jsf.common/src/org/eclipse/jst/jsf/context/AbstractDelegatingFactory.java
@@ -15,8 +15,8 @@ package org.eclipse.jst.jsf.context;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Iterator;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 import org.eclipse.core.runtime.IAdaptable;
 
@@ -26,85 +26,77 @@ import org.eclipse.core.runtime.IAdaptable;
  * Clients may extend this class.
  * 
  * @author cbateman
- *
+ * 
  */
-public abstract class AbstractDelegatingFactory implements IDelegatingFactory {
+public abstract class AbstractDelegatingFactory implements IDelegatingFactory
+{
 
-	/* instance attributes */
-	
-	/**
-	 * the list of registered factory delegates
-	 */
-	protected 	final List<IAdaptable>  _delegates;
-	
-	private 	final List<Class>		_supportedDelegates;
+    /* instance attributes */
 
-	/**
-	 * @param supportedDelegateTypes -- populates the list of classes used
-	 * by the isValidDelegate contract
-	 */
-	protected AbstractDelegatingFactory(Class[]  supportedDelegateTypes)
-	{
-		_delegates = new ArrayList<IAdaptable>();
-		
-		final List<Class> supportedTypes = new ArrayList<Class>();
-		supportedTypes.addAll(Arrays.asList(supportedDelegateTypes));
-		_supportedDelegates = Collections.unmodifiableList(supportedTypes);
-	}
+    /**
+     * the list of registered factory delegates
+     */
+    protected final CopyOnWriteArrayList<IAdaptable> _delegates;
 
-	/**
-	 * @see org.eclipse.jst.jsf.context.IDelegatingFactory#addFactoryDelegate(org.eclipse.core.runtime.IAdaptable)
-	 */
-	public final void addFactoryDelegate(IAdaptable delegate) 
-	{
-		synchronized(_delegates)
-		{
-			
-			if (!_delegates.contains(delegate)
-					&& isValidDelegate(delegate))
-			{
-				_delegates.add(delegate);
-			}
-		}
-	}
+    private final List<Class> _supportedDelegates;
 
-	/**
-	 * @see org.eclipse.jst.jsf.context.IDelegatingFactory#removeFactoryDelegate(org.eclipse.core.runtime.IAdaptable)
-	 */
-	public final boolean removeFactoryDelegate(IAdaptable delegate) 
-	{
-		synchronized(_delegates)
-		{
-			return _delegates.remove(delegate);
-		}	
-	}
+    /**
+     * @param supportedDelegateTypes
+     *            -- populates the list of classes used by the isValidDelegate
+     *            contract
+     */
+    protected AbstractDelegatingFactory(final Class[] supportedDelegateTypes)
+    {
+        _delegates = new CopyOnWriteArrayList<IAdaptable>();
 
-	/**
-	 * @see org.eclipse.jst.jsf.context.IDelegatingFactory#getValidDelegateTypes()
-	 */
-	public final List<Class> getValidDelegateTypes() 
-	{
-		return _supportedDelegates;
-	}
+        final List<Class> supportedTypes = new ArrayList<Class>();
+        supportedTypes.addAll(Arrays.asList(supportedDelegateTypes));
+        _supportedDelegates = Collections.unmodifiableList(supportedTypes);
+    }
 
-	/**
-	 * @see org.eclipse.jst.jsf.context.IDelegatingFactory#isValidDelegate(org.eclipse.core.runtime.IAdaptable)
-	 */
-	public final boolean isValidDelegate(IAdaptable delegate) 
-	{
-		for (final Iterator<Class> it = _supportedDelegates.iterator(); it.hasNext();)
-		{
-			final Class clazz = it.next();
-			
-			// if the delegate supports one of the valid delegate classes
-			// via adaptation, then it is a valid delegate
-			if (delegate.getAdapter(clazz) != null)
-			{
-				return true;
-			}
-		}
-		
-		// if no found, delegate is not supported
-		return false;
-	}
+    /**
+     * @see org.eclipse.jst.jsf.context.IDelegatingFactory#addFactoryDelegate(org.eclipse.core.runtime.IAdaptable)
+     */
+    public final void addFactoryDelegate(final IAdaptable delegate)
+    {
+        if (isValidDelegate(delegate))
+        {
+            _delegates.addIfAbsent(delegate);
+        }
+    }
+
+    /**
+     * @see org.eclipse.jst.jsf.context.IDelegatingFactory#removeFactoryDelegate(org.eclipse.core.runtime.IAdaptable)
+     */
+    public final boolean removeFactoryDelegate(final IAdaptable delegate)
+    {
+        return _delegates.remove(delegate);
+    }
+
+    /**
+     * @see org.eclipse.jst.jsf.context.IDelegatingFactory#getValidDelegateTypes()
+     */
+    public final List<Class> getValidDelegateTypes()
+    {
+        return _supportedDelegates;
+    }
+
+    /**
+     * @see org.eclipse.jst.jsf.context.IDelegatingFactory#isValidDelegate(org.eclipse.core.runtime.IAdaptable)
+     */
+    public final boolean isValidDelegate(final IAdaptable delegate)
+    {
+        for (final Class clazz : _supportedDelegates)
+        {
+            // if the delegate supports one of the valid delegate classes
+            // via adaptation, then it is a valid delegate
+            if (delegate.getAdapter(clazz) != null)
+            {
+                return true;
+            }
+        }
+
+        // if no found, delegate is not supported
+        return false;
+    }
 }
