a31f15e166a35da912c20d17c83224205bf9dbab
cbateman <cbateman>
2010-03-08 18:51:53 +0000

    Move facelet.ui into JSF Tools mainline from incubator.

---
+++ jsf/plugins/org.eclipse.jst.jsf.facelet.ui/src/org/eclipse/jst/jsf/facelet/ui/internal/facet/FaceletInstallPage.java
@@ -0,0 +1,18 @@
+package org.eclipse.jst.jsf.facelet.ui.internal.facet;
+
+
+/**
+ * The UI page for installing the Facelet facet.
+ * @author cbateman
+ *
+ */
+public class FaceletInstallPage extends FaceletChangePage
+{
+    /**
+     * 
+     */
+    public FaceletInstallPage()
+    {
+        super("Facelet Install Page");
+    }
+}
