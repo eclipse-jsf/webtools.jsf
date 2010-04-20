dff1ec943a0d55339ec36ecdce977a594efc7bb2
itrimble <itrimble>
2010-04-20 17:28:07 +0000

    Bug 213526 - [WPE] [JSF2.0] selectItem tags don't display in visual WPE page when page is JSPX

--- jsf/plugins/org.eclipse.jst.pagedesigner/src/org/eclipse/jst/pagedesigner/dtmanager/converter/operations/internal/CreateAttributeFromXPathOperation.java
+++ jsf/plugins/org.eclipse.jst.pagedesigner/src/org/eclipse/jst/pagedesigner/dtmanager/converter/operations/internal/CreateAttributeFromXPathOperation.java
@@ -10,6 +10,13 @@
  *******************************************************************************/ 
 package org.eclipse.jst.pagedesigner.dtmanager.converter.operations.internal;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.StringTokenizer;
+
+import javax.xml.namespace.NamespaceContext;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpressionException;
@@ -27,8 +34,12 @@ import org.w3c.dom.Element;
  */
 public class CreateAttributeFromXPathOperation extends AbstractTransformOperation {
 
+	private static final String DEFAULT_PREFIX = "prefix"; //$NON-NLS-1$
+
 	private String attributeName;
 	private String xPathExpression;
+	private String nsURI;
+	private String prefixedTokens;
 
 	/*
 	 * (non-Javadoc)
@@ -40,17 +51,28 @@ public class CreateAttributeFromXPathOperation extends AbstractTransformOperatio
 			getLog().error("Warning.TransformOperationFactory.TooFewParameters", getTransformOperationID()); //$NON-NLS-1$
 			return null;
 		}
-		
 		attributeName = getParameters()[0];
 		xPathExpression = getParameters()[1];				
 		Assert.isNotNull(attributeName);
 		Assert.isNotNull(xPathExpression);
+		if (getParameters().length > 3) {
+			nsURI = getParameters()[2];
+			Assert.isNotNull(nsURI);
+			prefixedTokens = getParameters()[3];
+			Assert.isNotNull(prefixedTokens);
+		}
 	
 		if (srcElement != null) {
 			XPath xPath = XPathFactory.newInstance().newXPath();
 			try {
 				Object resultObject = xPath.evaluate(xPathExpression, srcElement, XPathConstants.STRING);
 				if (resultObject instanceof String && curElement != null) {
+					if (((String)resultObject).equals("0")) { //$NON-NLS-1$
+						if (nsURI != null && prefixedTokens != null) {
+							xPath.setNamespaceContext(new NSContext(nsURI, DEFAULT_PREFIX));
+							resultObject = xPath.evaluate(getPrefixedExpression(DEFAULT_PREFIX), srcElement, XPathConstants.STRING);
+						}
+					}
 					curElement.setAttribute(attributeName, (String)resultObject);
 				}
 			} catch(XPathExpressionException xee) {
@@ -58,6 +80,45 @@ public class CreateAttributeFromXPathOperation extends AbstractTransformOperatio
 			}
 		}
 		return curElement;
+	}
+
+	private String getPrefixedExpression(String prefix) {
+		String prefixedExpression = xPathExpression;
+		final List<String> tokens = new ArrayList<String>();
+		final StringTokenizer tokenizer = new StringTokenizer(prefixedTokens, ","); //$NON-NLS-1$
+		while (tokenizer.hasMoreTokens()) {
+			tokens.add(tokenizer.nextToken());
+		}
+		for (String token: tokens) {
+			prefixedExpression = prefixedExpression.replace(token, prefix + ":" + token); //$NON-NLS-1$
+		}
+		return prefixedExpression;
+	}
+
+
+
+	class NSContext implements NamespaceContext {
+
+		private String _namespaceURI;
+		private String _prefix;
+
+		public NSContext(String namespaceURI, String prefix) {
+			_namespaceURI = namespaceURI;
+			_prefix = prefix;
+		}
+
+		public String getNamespaceURI(String prefix) {
+			return _namespaceURI;
+		}
+
+		public String getPrefix(String namespaceURI) {
+			return _prefix;
+		}
+
+		public Iterator getPrefixes(String namespaceURI) {
+			return Arrays.asList(new String[]{_prefix}).iterator();
+		}
+
 	}
 
 }
--- jsf/plugins/org.eclipse.jst.pagedesigner/src/org/eclipse/jst/pagedesigner/dtmanager/converter/operations/internal/IterateOverElementsOperation.java
+++ jsf/plugins/org.eclipse.jst.pagedesigner/src/org/eclipse/jst/pagedesigner/dtmanager/converter/operations/internal/IterateOverElementsOperation.java
@@ -10,6 +10,13 @@
  *******************************************************************************/ 
 package org.eclipse.jst.pagedesigner.dtmanager.converter.operations.internal;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.StringTokenizer;
+
+import javax.xml.namespace.NamespaceContext;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpressionException;
@@ -34,7 +41,11 @@ import org.w3c.dom.NodeList;
  */
 public class IterateOverElementsOperation extends AbstractTransformOperation {
 
+	private static final String DEFAULT_PREFIX = "prefix"; //$NON-NLS-1$
+
 	private String xPathExpression;
+	private String nsURI;
+	private String prefixedTokens;
 
 	/*
 	 * (non-Javadoc)
@@ -44,11 +55,16 @@ public class IterateOverElementsOperation extends AbstractTransformOperation {
 		if (getParameters().length < 1) {
 			getLog().error("Warning.TransformOperationFactory.TooFewParameters", getTransformOperationID()); //$NON-NLS-1$
 			return null;
-		} 
-			
+		}
 		xPathExpression = getParameters()[0];		
 		Assert.isNotNull(xPathExpression);
-		
+		if (getParameters().length > 2) {
+			nsURI = getParameters()[1];
+			Assert.isNotNull(nsURI);
+			prefixedTokens = getParameters()[2];
+			Assert.isNotNull(prefixedTokens);
+		}
+
 		Element retElement = curElement;
 		if (srcElement != null) {
 			XPath xPath = XPathFactory.newInstance().newXPath();
@@ -56,6 +72,15 @@ public class IterateOverElementsOperation extends AbstractTransformOperation {
 				Object resultObject = xPath.evaluate(xPathExpression, srcElement, XPathConstants.NODESET);
 				if (resultObject instanceof NodeList) {
 					NodeList nodes = (NodeList)resultObject;
+					if (nodes.getLength() < 1) {
+						if (nsURI != null && prefixedTokens != null) {
+							xPath.setNamespaceContext(new NSContext(nsURI, DEFAULT_PREFIX));
+							resultObject = xPath.evaluate(getPrefixedExpression(DEFAULT_PREFIX), srcElement, XPathConstants.NODESET);
+							if (resultObject instanceof NodeList) {
+								nodes = (NodeList)resultObject;
+							}
+						}
+					}
 					for (int i = 0; i < nodes.getLength(); i++) {
 						Node node = nodes.item(i);
 						if (node instanceof Element) {
@@ -68,6 +93,45 @@ public class IterateOverElementsOperation extends AbstractTransformOperation {
 			}
 		}
 		return retElement;
+	}
+
+	private String getPrefixedExpression(String prefix) {
+		String prefixedExpression = xPathExpression;
+		final List<String> tokens = new ArrayList<String>();
+		final StringTokenizer tokenizer = new StringTokenizer(prefixedTokens, ","); //$NON-NLS-1$
+		while (tokenizer.hasMoreTokens()) {
+			tokens.add(tokenizer.nextToken());
+		}
+		for (String token: tokens) {
+			prefixedExpression = prefixedExpression.replace(token, prefix + ":" + token); //$NON-NLS-1$
+		}
+		return prefixedExpression;
+	}
+
+
+
+	class NSContext implements NamespaceContext {
+
+		private String _namespaceURI;
+		private String _prefix;
+
+		public NSContext(String namespaceURI, String prefix) {
+			_namespaceURI = namespaceURI;
+			_prefix = prefix;
+		}
+
+		public String getNamespaceURI(String prefix) {
+			return _namespaceURI;
+		}
+
+		public String getPrefix(String namespaceURI) {
+			return _prefix;
+		}
+
+		public Iterator getPrefixes(String namespaceURI) {
+			return Arrays.asList(new String[]{_prefix}).iterator();
+		}
+
 	}
 
 }
