3dc1f36d0aed73becdae865e09a67eb9354c37f5
cbateman <cbateman>
2010-05-21 19:33:45 +0000

    Test-only addition of some documentation to BugRegressionTest annotation.

--- jsf/tests/org.eclipse.jst.jsf.test.util/src/org/eclipse/jst/jsf/test/util/junit4/BugRegressionTest.java
+++ jsf/tests/org.eclipse.jst.jsf.test.util/src/org/eclipse/jst/jsf/test/util/junit4/BugRegressionTest.java
@@ -2,7 +2,13 @@ package org.eclipse.jst.jsf.test.util.junit4;
 
 public @interface BugRegressionTest
 {
+    // can be used to specify what bug system the bug was reported in.
+    // default == Eclipse
     String bugSystem() default "Eclipse";
 
+    /**
+     * @return the bug number that that annotated test method covers
+     * regression testing for.
+     */
     long bugNumber();
 }
