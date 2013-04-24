9da19d805637ac305e13e46160b97a26a3216518
cbateman <cameron.bateman@oracle.com>
2013-04-24 10:10:34 -0700

    Fix for fix for 390867 per reopen.

--- jsf/plugins/org.eclipse.jst.jsf.core/src/org/eclipse/jst/jsf/core/internal/contentassist/el/ContentAssistParser.java
+++ jsf/plugins/org.eclipse.jst.jsf.core/src/org/eclipse/jst/jsf/core/internal/contentassist/el/ContentAssistParser.java
@@ -116,7 +116,7 @@ public final class ContentAssistParser
 	}
     
     private static String substring(String s, Region r) {
-        if (s == null && s.isEmpty())
+        if (s == null || s.isEmpty())
         {
             return ""; //$NON-NLS-1$
         }
