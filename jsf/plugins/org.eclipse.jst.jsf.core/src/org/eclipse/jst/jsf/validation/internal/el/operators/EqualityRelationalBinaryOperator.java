508d767cb60a6c56ac4cb8ae2ba18bf553c76154
Nitin Dahyabhai <thatnitind@gmail.com>
2023-04-05 00:00:50 -0400

    [581677] Update copyright on EqualityRelationalBinaryOperator.java
    Change-Id: Ic4a782d0f7e746d7c6c4c303092bccf4481a98a9

--- jsf/plugins/org.eclipse.jst.jsf.core/src/org/eclipse/jst/jsf/validation/internal/el/operators/EqualityRelationalBinaryOperator.java
+++ jsf/plugins/org.eclipse.jst.jsf.core/src/org/eclipse/jst/jsf/validation/internal/el/operators/EqualityRelationalBinaryOperator.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2010 Oracle Corporation.
+ * Copyright (c) 2006, 2023 Oracle Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -9,7 +9,7 @@
  *
  * Contributors:
  *    Cameron Bateman/Oracle - initial API and implementation
- *    
+ *    Reto Weiss <reto.weiss@axonivy.com> - Bug 581677
  ********************************************************************************/
 
 package org.eclipse.jst.jsf.validation.internal.el.operators;
