3af018d7f362675d94169ef61930d985a3933528
Vladimir V. Bychkov <github@bychkov.name>
2025-02-12 19:33:11 +0100

    feat: add additionalInfo for convertDateTime.pattern

--- jsf/plugins/org.eclipse.jst.jsf.core/src/org/eclipse/jst/jsf/taglibprocessing/attributevalues/DateTimePatternType.java
+++ jsf/plugins/org.eclipse.jst.jsf.core/src/org/eclipse/jst/jsf/taglibprocessing/attributevalues/DateTimePatternType.java
@@ -13,11 +13,18 @@
  ********************************************************************************/
 package org.eclipse.jst.jsf.taglibprocessing.attributevalues;
 
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.eclipse.jst.jsf.context.resolver.structureddocument.IDOMContextResolver;
 import org.eclipse.jst.jsf.context.resolver.structureddocument.IStructuredDocumentContextResolverFactory;
+import org.eclipse.jst.jsf.core.internal.JSFCorePlugin;
 import org.eclipse.jst.jsf.metadataprocessors.AbstractRootTypeDescriptor;
 import org.eclipse.jst.jsf.metadataprocessors.features.IPossibleValues;
 import org.eclipse.jst.jsf.metadataprocessors.features.PossibleValue;
@@ -46,24 +53,64 @@ public class DateTimePatternType extends AbstractRootTypeDescriptor implements I
 	};
 
 	public List getPossibleValues() {
-		List ret = new ArrayList();		
+		List<String> ret = new ArrayList();
 		String typeVal = getTypeValue();
-		if ("date".equalsIgnoreCase(typeVal)){ //$NON-NLS-1$
+		if ("date".equalsIgnoreCase(typeVal) || "localDate".equalsIgnoreCase(typeVal)) { //$NON-NLS-1$ //$NON-NLS-2$
 			for (int i=0;i<DATEPATTERNS.length;i++){
-				ret.add(new PossibleValue(DATEPATTERNS[i]));
+				ret.add(DATEPATTERNS[i]);
 			}
 		}
-		else if ("time".equalsIgnoreCase(typeVal)){ //$NON-NLS-1$
+		else if ("time".equalsIgnoreCase(typeVal) || "localTime".equalsIgnoreCase(typeVal) || //$NON-NLS-1$ //$NON-NLS-2$
+				"offsetTime".equalsIgnoreCase(typeVal)) { //$NON-NLS-1$
 			for (int i=0;i<TIMEPATTERNS.length;i++){
-				ret.add(new PossibleValue(TIMEPATTERNS[i]));
+				ret.add(TIMEPATTERNS[i]);
 			}
 		}
 		else {	
 			for (int i=0;i<DATETIMEPATTERNS.length;i++){
-				ret.add(new PossibleValue(DATETIMEPATTERNS[i]));
+				ret.add(DATETIMEPATTERNS[i]);
 			}
 		}
-		return ret;
+		List<PossibleValue> result = new ArrayList<>();
+		result.addAll(ret.stream().map(item -> toPossibleValue(item, false)).collect(Collectors.toList()));
+		String currentValue = getCurrentAttrValue();
+		if (currentValue != null && !currentValue.trim().isEmpty() && !ret.contains(currentValue.trim())) {
+			result.add(0, toPossibleValue(currentValue, true));
+		}
+		return result;
+	}
+	
+	private static List<ZonedDateTime> dates;
+	private static List<ZonedDateTime> getExampleDates() {
+		if (dates == null) {
+			dates = Arrays.asList(
+					ZonedDateTime.now(),
+					ZonedDateTime.now().plus(13, ChronoUnit.YEARS).plus(6, ChronoUnit.MONTHS).plus(12, ChronoUnit.HOURS).plus(34, ChronoUnit.MINUTES),
+					ZonedDateTime.of(1903, 12, 28, 19, 26, 15, 123456789, ZoneId.systemDefault()),
+					ZonedDateTime.of(1600, 2, 17, 9, 5, 37, 123456789, ZoneId.systemDefault()),
+					ZonedDateTime.of(-4, 7, 15, 7, 42, 5, 123456789, ZoneId.systemDefault())
+				);
+		}
+		return dates;
+	}
+	
+	private PossibleValue toPossibleValue(String value, boolean showError) {
+		PossibleValue pValue = new PossibleValue(value);
+		try {
+			DateTimeFormatter dtf = DateTimeFormatter.ofPattern(value);
+			StringBuilder sb = new StringBuilder("Examples:<br>"); //$NON-NLS-1$
+			sb.append(getExampleDates().stream().map(item -> " - " + item.format(dtf)) //$NON-NLS-1$
+					.collect(Collectors.joining("<br>"))); //$NON-NLS-1$
+			pValue.setAdditionalInformation(sb.toString());
+		} catch (Throwable e) {
+			if (showError) {
+				String additionalInfo = String.format("Pattern is invalid: %s", e.getMessage()); //$NON-NLS-1$
+				pValue.setAdditionalInformation(additionalInfo);
+			} else {
+				JSFCorePlugin.log(e.getMessage(), e);
+			}
+		}
+		return pValue;
 	}
 	
 	private String getTypeValue() {
@@ -82,5 +129,14 @@ public class DateTimePatternType extends AbstractRootTypeDescriptor implements I
 		return ""; //$NON-NLS-1$
 	}
 
+	private String getCurrentAttrValue() {
+		if (getStructuredDocumentContext() != null){
+			IDOMContextResolver resolver = IStructuredDocumentContextResolverFactory.INSTANCE.getDOMContextResolver(getStructuredDocumentContext());
+			if (resolver != null){
+				return resolver.getNode().getNodeValue();
+			}
+		}
+		return ""; //$NON-NLS-1$
 
+	}
 }
